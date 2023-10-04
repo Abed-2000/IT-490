d8f7ed3bb8d17579b9183c74e8a5c5dcc89fae0c
Abed Miah <abedmia2000@gmail.com>
2023-10-03 20:16:23 -0400

    Modified: Something...

--- validateLogin.php
+++ validateLogin.php
@@ -2,17 +2,29 @@
 
 require_once('databaseConnection.inc');
 
-function validateLogin(){
-    $database = Connection();
-    $clean_username = database -> real_escape_string($username);
-    $clean_password = database -> real_escape_string($password);
-    $query = "SELECT * FROM users WHERE (`Username` = '$clean_username') AND (`Password` = '$clean_password')";
+function validateLogin($database, $user, $pass){
+    $username = $database->real_escape_string($user);
+    $password = $database->real_escape_string($pass);
+    $query = "SELECT * FROM users WHERE `Username` = '$username'";
+
     $result = database -> query($query);
 
     if (!$result) {
         die("Query failed: " . $database->error);
     }
     
+	while ($row = $response->fetch_assoc())
+	{
+		echo "checking password for $username".PHP_EOL;
+		if ($row["Password"] == $password)
+		{
+			echo "passwords match for $username".PHP_EOL;
+			return 1;// password match
+		}
+		echo "passwords did not match for $username".PHP_EOL;
+	}
+	return 0;//no users matched username
+
     $database->close();
 
 }
