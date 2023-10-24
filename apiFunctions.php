78c596dc855afc6c57f4ec7e0d4cb5e8645d4944
Abed Miah <abedmia2000@gmail.com>
2023-10-24 19:50:10 -0400

    Added: getMealDetailsfunction

--- apiFunctions.php
+++ apiFunctions.php
@@ -29,17 +29,31 @@ function searchMeals($query) {
 }
 
 function getMealDetails($query){
-    $apiEndpoint = "www.themealdb.com/api/json/v1/1/lookup.php?i=" . urlencode($query);
-    $ch = curl_init($apiEndpoint);
+    
+    $apiEndpoint = "https://www.themealdb.com/api/json/v1/1/lookup.php?i=" . $query;
+    $ch = curl_init();
+    curl_setopt($ch, CURLOPT_URL, $apiEndpoint);
     curl_setopt($ch, CURLOPT_RETURNTRANSFER, true);
+
     $response = curl_exec($ch);
 
-    if ($response === false) {
-        die('cURL Error: ' . curl_error($ch));
+    if (curl_errno($ch)) {
+        return array("returnCode" => 0, "message" => "Curl error: " . curl_error($ch));
     }
-
     curl_close($ch);
 
-    $data = json_decode($response, true);
+    $mealData = json_decode($response, true);
+
+    if ($mealData === null) {
+        return array("returnCode" => 0, "message" => "Unable to decode the JSON data.");
+    }
+
+    if (isset($mealData['meals'][0])) {
+        return array("returnCode" => 1, "message" => $mealData['meals'][0]);
+    } else {
+        return array("returnCode" => 0, "message" => "Error Retrieving data from the API.");
+    }
 }
+
+
 ?>
