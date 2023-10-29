8f9ca7e2a4c7c6e6d2cc9bfc8bf5f4c7f7cc019c
Abed Miah <abedmia2000@gmail.com>
2023-10-29 16:30:17 -0400

    custom recipe save to db

--- sessions_handler.php
+++ sessions_handler.php
@@ -138,3 +138,44 @@ function rateRecipe($mealID, $accountID, $rating)
             }
         }
     } 
+
+    function saveRecipe($formDataArray) {
+
+        global $dbHost, $dbUsername, $dbPassword, $dbName;
+        $conn = new mysqli($dbHost, $dbUsername, $dbPassword, $dbName);
+    
+        if ($conn->connect_error) {
+            echo "Error connecting to database: " . $conn->connect_error . PHP_EOL;
+            exit(1);
+        }
+
+        foreach ($formDataArray as $formData) {
+            $sql = "INSERT INTO custom_meals (strMeal, strCategory, strArea, strInstructions, strMealThumb, strTags, 
+                strIngredient1, strIngredient2, strIngredient3, strIngredient4, strIngredient5, strIngredient6, strIngredient7, 
+                strIngredient8, strIngredient9, strIngredient10, strIngredient11, strIngredient12, strIngredient13, strIngredient14, 
+                strIngredient15, strIngredient16, strIngredient17, strIngredient18, strIngredient19, strIngredient20, strMeasure1, 
+                strMeasure2, strMeasure3, strMeasure4, strMeasure5, strMeasure6, strMeasure7, strMeasure8, strMeasure9, strMeasure10, 
+                strMeasure11, strMeasure12, strMeasure13, strMeasure14, strMeasure15, strMeasure16, strMeasure17, strMeasure18, 
+                strMeasure19, strMeasure20, strYoutube, datePublished, created_by) 
+                VALUES (?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, 
+                ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?)";
+    
+            $stmt = $conn->prepare($sql);
+            $stmt->bind_param("ssssssssssssssssssssssssssssssssssssssssssssssssssssssssssssssssssssssssssssssssssssss",
+                $formData['strMeal'], $formData['strCategory'], $formData['strArea'], $formData['strInstructions'], $formData['strMealThumb'], $formData['strTags'],
+                $formData['strIngredients'][0], $formData['strIngredients'][1], $formData['strIngredients'][2], $formData['strIngredients'][3], $formData['strIngredients'][4],
+                $formData['strIngredients'][5], $formData['strIngredients'][6], $formData['strIngredients'][7], $formData['strIngredients'][8], $formData['strIngredients'][9],
+                $formData['strIngredients'][10], $formData['strIngredients'][11], $formData['strIngredients'][12], $formData['strIngredients'][13], $formData['strIngredients'][14],
+                $formData['strIngredients'][15], $formData['strIngredients'][16], $formData['strIngredients'][17], $formData['strIngredients'][18], $formData['strIngredients'][19],
+                $formData['strMeasure'][0], $formData['strMeasure'][1], $formData['strMeasure'][2], $formData['strMeasure'][3], $formData['strMeasure'][4],
+                $formData['strMeasure'][5], $formData['strMeasure'][6], $formData['strMeasure'][7], $formData['strMeasure'][8], $formData['strMeasure'][9],
+                $formData['strMeasure'][10], $formData['strMeasure'][11], $formData['strMeasure'][12], $formData['strMeasure'][13], $formData['strMeasure'][14],
+                $formData['strMeasure'][15], $formData['strMeasure'][16], $formData['strMeasure'][17], $formData['strMeasure'][18], $formData['strMeasure'][19],
+                $formData['strYoutube'], $formData['datePublished'], $formData['created_by']);
+    
+            $stmt->execute();
+            $stmt->close();
+        }
+        $conn->close();
+        return array("returnCode" => 1, "message" => "Meal was successfully saved.");
+    }
